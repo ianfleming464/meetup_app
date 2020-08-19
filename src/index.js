35184a2b30f1f2df3d33719067291718116f3dc2
ianfleming464 <60020259+ianfleming464@users.noreply.github.com>
2020-08-19 07:02:26 +0200

    Registered service worker, fixed icon bug, added a little styling

--- src/index.js
+++ src/index.js
@@ -15,5 +15,5 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register();
 atatus.config("3bc6025c5f6143b397f066be2e9ee98d").install();
