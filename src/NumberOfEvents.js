a596355d3b7a0cd12c8adfab909884011dc533a8
ianfleming464 <60020259+ianfleming464@users.noreply.github.com>
2020-08-25 21:41:58 +0200

    4.9 Added first chart as per task

--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -9,23 +9,25 @@ export default class NumberOfEvents extends Component {
 
   handleInputChanged = (event) => {
     const value = event.target.value;
-    this.setState({ eventNumber: value });
 
     if (value <= 0) {
       this.setState({
         errorText: "Number should be at least 1!",
       });
+      // alert("Look!");
     } else {
       this.setState({
         errorText: "",
       });
+      this.setState({ eventNumber: value });
+      this.props.updateEvents(null, null, value);
     }
-    this.props.updateEvents(null, null, value);
   };
+
   render() {
     return (
       <div className="numberOfEvents">
-        <label>Events: </label>
+        <span>Events: </span>
         <input className="number-of-events" type="number" value={this.state.eventNumber} onChange={this.handleInputChanged} />
         <div className="alerts">
           <ErrorAlert text={this.state.errorText} />
